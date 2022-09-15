bfc7032978e7c6dafbca6c7dac79c5de482b5164
Nguyen Anh Quan <quanna@regit-technology.com>
2022-09-15 11:30:58 +0700

    update responsive @mixin file

--- webpack-demo/src/components/comment.js
+++ webpack-demo/src/components/comment.js
@@ -5,13 +5,13 @@ const Comment = {
       <div class="comment-post-title">
           <h4 class="">Comment</h4>
       </div>
-      <div class="type-row d-flex align-items-center gap-3">
-        <div class="type-row__avatar flex-shrink-2"><img class="img-fluid" src="./assets/img/avatar2.png"/></div>
-        <div class="type-row__input flex-grow-1"><input type="text" /></div>
+      <div class="type-row">
+        <div class="type-row__avatar"><img class="img-fluid" src="./assets/img/avatar2.png"/></div>
+        <div class="type-row__input"><input type="text" /></div>
       </div>
       <div class="list-comment-row mt-5 mb-5">
         <div class="list-comment-row__avatar"><img class="img-fluid" src="./assets/img/avatar2.png"/></div>
-        <div class="list-comment-row__text flex-grow-1">
+        <div class="list-comment-row__text">
           <div class="comment-name">Your Name</div>
           <p>Yes,it's right</p>
           <div class="comment-time">
